b1018f02b5ff486720b8bc662e6a05ccaa9c8385
Yuheng-Li <yhnli@ucdavis.edu>
2024-06-28 16:39:04 -0700

    xx

--- scripts/v1_5/finetune_task.sh
+++ scripts/v1_5/finetune_task.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export WANDB_PROJECT='edit_instruction_follow'
 
+export NAME='llava-v1.5-13b-init_try_700k_mask'
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-13b \
@@ -16,7 +17,49 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/trash \
+    --output_dir ./checkpoints/${NAME} \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name ${NAME} \
+
+
+
+
+
+export NAME='trash'
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path liuhaotian/llava-v1.5-13b \
+    --version v1 \
+    --data_path ../edit_instruction_follow_data/llava_filtering_700k.json \
+    --image_folder s3://myedit-cz/upsample_1k/_nomask/masked_results/stock5M_ediffiN130_v1/merge_three_segs/ \
+    --mask_folder s3://myedit-cz/merge_three_segs/stock5M_ediffiN130_v1/ \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ./checkpoints/${NAME} \
     --num_train_epochs 100 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -36,48 +79,7 @@ deepspeed llava/train/train_mem.py \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
-    --run_name trash \
-
-
-
-
-
-
-# CUDA_VISIBLE_DEVICES=0 python llava/train/train_mem.py \
-#     --model_name_or_path liuhaotian/llava-v1.5-13b \
-#     --version v1 \
-#     --data_path ../edit_instruction_follow_data/llava_filtering_700k.json \
-#     --image_folder s3://myedit-cz/upsample_1k/_nomask/masked_results/stock5M_ediffiN130_v1/merge_three_segs/ \
-#     --mask_folder s3://myedit-cz/merge_three_segs/stock5M_ediffiN130_v1/ \
-#     --vision_tower openai/clip-vit-large-patch14-336 \
-#     --mm_projector_type mlp2x_gelu \
-#     --mm_vision_select_layer -2 \
-#     --mm_use_im_start_end False \
-#     --mm_use_im_patch_token False \
-#     --image_aspect_ratio pad \
-#     --group_by_modality_length True \
-#     --bf16 True \
-#     --output_dir ./checkpoints/trash \
-#     --num_train_epochs 100 \
-#     --per_device_train_batch_size 16 \
-#     --per_device_eval_batch_size 4 \
-#     --gradient_accumulation_steps 1 \
-#     --evaluation_strategy "no" \
-#     --save_strategy "steps" \
-#     --save_steps 50000 \
-#     --save_total_limit 1 \
-#     --learning_rate 2e-5 \
-#     --weight_decay 0. \
-#     --warmup_ratio 0.03 \
-#     --lr_scheduler_type "cosine" \
-#     --logging_steps 1 \
-#     --tf32 True \
-#     --model_max_length 2048 \
-#     --gradient_checkpointing True \
-#     --dataloader_num_workers 4 \
-#     --lazy_preprocess True \
-#     --report_to wandb \
-#     --run_name trash \
+    --run_name ${NAME} \
 
 
 
@@ -95,38 +97,3 @@ deepspeed llava/train/train_mem.py \
 
 
 
-# deepspeed llava/train/train_mem.py \
-#     --deepspeed ./scripts/zero3.json \
-#     --model_name_or_path liuhaotian/llava-v1.5-13b \
-#     --version v1 \
-#     --data_path ../edit_instruction_follow_data/llava_filtering_100k.json \
-#     --image_folder s3://myedit-cz/upsample_1k/_nomask/masked_results/stock5M_ediffiN130_v1/merge_three_segs/ \
-#     --vision_tower openai/clip-vit-large-patch14-336 \
-#     --mm_projector_type mlp2x_gelu \
-#     --mm_vision_select_layer -2 \
-#     --mm_use_im_start_end False \
-#     --mm_use_im_patch_token False \
-#     --image_aspect_ratio pad \
-#     --group_by_modality_length True \
-#     --bf16 True \
-#     --output_dir ./checkpoints/trash \
-#     --num_train_epochs 10 \
-#     --per_device_train_batch_size 16 \
-#     --per_device_eval_batch_size 4 \
-#     --gradient_accumulation_steps 1 \
-#     --evaluation_strategy "no" \
-#     --save_strategy "steps" \
-#     --save_steps 50000 \
-#     --save_total_limit 1 \
-#     --learning_rate 2e-5 \
-#     --weight_decay 0. \
-#     --warmup_ratio 0.03 \
-#     --lr_scheduler_type "cosine" \
-#     --logging_steps 1 \
-#     --tf32 True \
-#     --model_max_length 2048 \
-#     --gradient_checkpointing True \
-#     --dataloader_num_workers 4 \
-#     --lazy_preprocess True \
-#     --report_to wandb \
-#     --run_name trash \
